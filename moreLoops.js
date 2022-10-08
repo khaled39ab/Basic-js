d6e66524a59562552dc6f9f9e18f4a00f83f7f4e
khaled39ab <khaled39ab@gmail.com>
2022-10-08 19:08:35 +0600

    sorting array with sort function

--- moreLoops.js
+++ moreLoops.js
@@ -1,48 +1,50 @@
-// for (let i = 0; i < 5; i++){
-//     console.log(i);
-// }
+for (let i = 0; i < 5; i++) {
+    // console.log(i);
+}
 
-// const arr = [23, 43, 54, 39]
-// for (let i = 0; i < arr.length; i++) {
-//     const element = arr[i];
-//     console.log(element);
-// }
+const arr = [23, 43, 54, 39]
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    // console.log(element);
+}
 
-// const marks = [69, 45, 98, 75, 91, 99, 85];
-// let max = 0;
-// let min = marks[0];
-// for (let i = 0; i < marks.length; i++) {
-//     const element = marks[i];
-//     if (element > max) {
-//         max = element;
-//     } 
-//     if (element < min) {
-//         min = element;
-//     }
-// }
+const marks = [69, 45, 98, 75, 91, 99, 85];
+let max = 0;
+let min = marks[0];
+for (let i = 0; i < marks.length; i++) {
+    const element = marks[i];
+    if (element > max) {
+        max = element;
+    }
+    if (element < min) {
+        min = element;
+    }
+}
 // console.log ("Highest value is: ", max);
 // console.log ("Lowest value is: ", min);
 
-/* let sort = [];
 
-for (let i = 0; i < marks.length; i++){
-    const ele = marks[0];
-    if (ele > min){
-        sort.push(min);
-    }
-}
-
-console.log(sort); */
 const num = [13, 17, 14, 19, 16];
-let temp;
+let temp = 0;
 for (let i = 0; i < num.length; i++) {
-    for(let j=i+1;j<num.length;j++){
-        if(num[i]>num[j]){
-            temp=num[i]
-            num[i]=num[j]
-            num[j]=temp
+    for (let j = i + 1; j < num.length; j++) {
+        if (num[i] > num[j]) {
+            temp = num[i]
+            num[i] = num[j]
+            num[j] = temp
         }
     }
 }
 
-console.log(num);
+// console.log(num);
+
+/* 
+---------------------------------------   sorting with sort function
+*/
+
+const number = [43, 54, 23, 65, 75, 21, 32]
+number.sort(function (a, b) {
+    return a - b
+})
+
+console.log(number);
