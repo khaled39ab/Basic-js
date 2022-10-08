1d3cbf54ba7b9c6ff866eeb5d35245aa2e6c6382
khaled39ab <khaled39ab@gmail.com>
2022-10-08 11:34:13 +0600

    sorting array

--- moreLoops.js
+++ moreLoops.js
@@ -8,17 +8,41 @@
 //     console.log(element);
 // }
 
-const marks = [69, 45, 98, 75, 91, 99, 85];
-let max = 0;
+// const marks = [69, 45, 98, 75, 91, 99, 85];
+// let max = 0;
 // let min = marks[0];
-for (let i = 0; i < marks.length; i++) {
-    const element = marks[i];
-    if (element > max) {
-        max = element;
-    } 
-    // if (element < min) {
-    //     min = element;
-    // }
-}
-console.log ("Highest value is: ", max);
+// for (let i = 0; i < marks.length; i++) {
+//     const element = marks[i];
+//     if (element > max) {
+//         max = element;
+//     } 
+//     if (element < min) {
+//         min = element;
+//     }
+// }
+// console.log ("Highest value is: ", max);
 // console.log ("Lowest value is: ", min);
+
+/* let sort = [];
+
+for (let i = 0; i < marks.length; i++){
+    const ele = marks[0];
+    if (ele > min){
+        sort.push(min);
+    }
+}
+
+console.log(sort); */
+const num = [13, 17, 14, 19, 16];
+let temp;
+for (let i = 0; i < num.length; i++) {
+    for(let j=i+1;j<num.length;j++){
+        if(num[i]>num[j]){
+            temp=num[i]
+            num[i]=num[j]
+            num[j]=temp
+        }
+    }
+}
+
+console.log(num);
